a10142605cdfda00d5d0b72fd09311c8462f2bc2
juliemeta <juliemonefeldt_404@hotmail.com>
2024-10-17 11:20:06 +0200

    Added fully functional AI API integration to js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+function displayRecipe(response) {
+  console.log("recipe generated");
+
+  new Typewriter("#generated-recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 15,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#generated-recipe", {
-    strings: "AI generated recipe",
-    autoStart: true,
-    delay: 20,
-  });
+  ingredientInput = document.querySelector("#ingredient-input");
+  let apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
+  let context =
+    "You are a vegetarian AI assistant who likes to cook simple vegetarian dishes. The recipes you generate must not contain any meat or seafood. If you are asked to include a non-vegetarian ingedient in your recipe, you will not do so. Make sure to display the recipe in a user friendly way by using basic HTML. Give the recipe a title. If you want to, you can finish by saying something like: `Bon appétit!` and include an emoji of your choice.";
+  let prompt = `Please generate a vegetarian recipe. ${ingredientInput.value} must be included as an ingredient in the recipe. The recipe must be divided into step 1, step 2 etc.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(
+    `generating vegetarian recipe containing ${ingredientInput.value}`
+  );
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 new Typewriter("#generated-recipe", {
