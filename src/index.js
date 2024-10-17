0c4c28282c8e771aa34215935a46df6fe953ef6c
juliemeta <juliemonefeldt_404@hotmail.com>
2024-10-17 11:50:18 +0200

    Optimised AI context and also improved UX by adding waiting message

--- src/index.js
+++ src/index.js
@@ -14,9 +14,15 @@ function generateRecipe(event) {
   ingredientInput = document.querySelector("#ingredient-input");
   let apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
   let context =
-    "You are a vegetarian AI assistant who likes to cook simple vegetarian dishes. The recipes you generate must not contain any meat or seafood. If you are asked to include a non-vegetarian ingedient in your recipe, you will not do so. Make sure to display the recipe in a user friendly way by using basic HTML. Give the recipe a title. If you want to, you can finish by saying something like: `Bon appétit!` and include an emoji of your choice.";
+    "You are a vegetarian AI assistant who likes to cook simple vegetarian dishes. The recipes you generate must not contain any meat or seafood. If you are asked to include a non-vegetarian ingredient in your recipe, you will not do so. Be creative and show a new recipe if someone asks for the same ingredient twice. Make sure to display the recipe in a user friendly way by using basic HTML, but do NOT write the word `html` anywhere. Give the recipe a title. If you want to, you can finish by saying something like: `Bon appétit!` and include an emoji of your choice.";
   let prompt = `Please generate a vegetarian recipe. ${ingredientInput.value} must be included as an ingredient in the recipe. The recipe must be divided into step 1, step 2 etc.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  new Typewriter("#generated-recipe", {
+    strings: "Generating recipe for you...",
+    autoStart: true,
+    delay: 100,
+  });
 
   console.log(
     `generating vegetarian recipe containing ${ingredientInput.value}`
